2b59432767977893c4e01e971d2d62094d5c7ea6
Danilo <dan.varushchik@gmail.com>
2019-10-20 02:03:20 +0300

    Initial Commit

---
+++ ColorSwitch/GameViewController.swift
@@ -0,0 +1,32 @@
+//
+//  GameViewController.swift
+//  ColorSwitch
+//
+//  Created by Daniel Varushchyk on 20.10.2019.
+//  Copyright © 2019 DanVar. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            let scene = GameScene(size: view.bounds.size)
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
